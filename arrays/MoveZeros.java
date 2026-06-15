98990864988bb62128692288a2ccf4e70ebe2c6b
arushimalay <arushimalay@gmail.com>
2026-06-15 21:34:45 +0530

    Move Zeros

---
+++ arrays/MoveZeros.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public void moveZeroes(int[] nums) 
+    {
+        int size=nums.length;
+        int a=0;
+        int b=size-1;
+        int i;
+        for(i=0;i<size;i++)
+        {
+            if (nums[i]!=0)
+            {
+                nums[a++]=nums[i];
+            }
+        }
+        for(i=a;i<size;i++)
+        {
+            nums[i]=0;
+        }
+    }
+}
