8931966acf6e1440c73c73ef3000853ae47636a5
arushimalay <arushimalay@gmail.com>
2026-06-19 18:13:33 +0530

    Using HashSet

---
+++ arrays/ContainsDuplicate.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) 
+    {
+        HashSet<Integer> dup=new HashSet<>();
+        for(int num:nums)
+        {
+            if(dup.contains(num))
+            return true;
+            dup.add(num);
+        }
+        return false;
+    }
+}
